c8475b5cf79bbcb52a13ee64c6387ac4a8f87b20
Kai Katajisto <kai.katajisto@seamk.fi>
2026-02-03 10:53:48 +0200

    Docker Test #1

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello Docker World!";
+        return "Hello World from VSC to GitHub to Docker image in csc-server!";
     }
 }
